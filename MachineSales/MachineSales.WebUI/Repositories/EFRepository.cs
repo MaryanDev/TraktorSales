2176a33d2e5858de525cf9c5f57abf500302959c
Maryan Maykher <mr.maykher@gmail.com>
2016-12-07 21:12:38 +0200

    Provided data access

---
+++ MachineSales/MachineSales.WebUI/Repositories/EFRepository.cs
@@ -0,0 +1,54 @@
+﻿using MachineSales.WebUI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MachineSales.WebUI.Repositories
+{
+    public class EFRepository
+    {
+        private traktorsSalesDbEntities context;
+        public EFRepository()
+        {
+            context = new traktorsSalesDbEntities();
+        }
+        public void Delete<TEntity>(TEntity entity) where TEntity : class
+        {
+            context.Entry(entity).State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public List<TEntity> Get<TEntity>(Func<TEntity, bool> criteria = null) where TEntity : class
+        {
+            IQueryable<TEntity> query = context.Set<TEntity>();
+            if (criteria != null)
+            {
+                query = query.Where(criteria).AsQueryable();
+            }
+            return query.ToList();
+        }
+
+        public TEntity GetSingle<TEntity>(Func<TEntity, bool> criteria) where TEntity : class
+        {
+            IQueryable<TEntity> query = context.Set<TEntity>();
+            query = query.Where(criteria).AsQueryable();
+            return query.SingleOrDefault();
+        }
+
+        public TEntity Insert<TEntity>(TEntity entity) where TEntity : class
+        {
+            context.Set<TEntity>().Add(entity);
+            context.SaveChanges();
+            return entity;
+        }
+
+        public TEntity Update<TEntity>(TEntity entity) where TEntity : class
+        {
+            context.Entry<TEntity>(entity).State = EntityState.Modified;
+            context.SaveChanges();
+            return entity;
+        }
+    }
+}
