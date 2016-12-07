444d7432ea815663bf9f005255eae551a219d043
Maryan Maykher <mr.maykher@gmail.com>
2016-12-07 21:28:06 +0200

    added main controllers, also added layout and loginModel

---
+++ MachineSales/MachineSales.WebUI/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MachineSales.WebUI.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Логін")]
+        public string Login { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
