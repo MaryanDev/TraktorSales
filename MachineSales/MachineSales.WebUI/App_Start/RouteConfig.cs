1e5eeb700cb1fe391f7b1839b6f9e4f13c975933
Maryan Maykher <mr.maykher@gmail.com>
2017-01-03 12:58:15 +0200

    second revert

--- MachineSales/MachineSales.WebUI/App_Start/RouteConfig.cs
+++ MachineSales/MachineSales.WebUI/App_Start/RouteConfig.cs
@@ -14,14 +14,25 @@ namespace MachineSales.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 <<<<<<< HEAD
+<<<<<<< HEAD
             routes.MapMvcAttributeRoutes();
 =======
 >>>>>>> 3bc9d2f66f8f3fee64c703b21da8ca4e70142f78
+=======
+            routes.MapMvcAttributeRoutes();
+
+>>>>>>> parent of 60e25ae... merge needed
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Content", action = "Home", id = UrlParameter.Optional }
             );
+
+            //routes.MapRoute(
+            //    name: "Admin",
+            //    url: "admin/",
+            //    defaults: new { controller = "Admin", action = "Dashboard" }
+            //);
         }
     }
 }
---
+++ MachineSales/MachineSales.WebUI/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using MachineSales.WebUI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MachineSales.WebUI.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected EFRepository _repository;
+
+        public BaseController()
+        {
+            _repository = new EFRepository();
+            pageSize = 5;
+        }
+
+        protected virtual int pageSize { get; set; }
+
+        protected int GetCountOfPages(int allPages, int size)
+        {
+            var pages = allPages / size;
+            var count = allPages % size == 0 ? pages : ++pages;
+            return count;
+        }
+    }
+}
